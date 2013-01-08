3a495e85dbfa2447ea69ebf7ae93db30a5da691e
gerard toko <gerardtoko@gmail.com>
2013-01-08 03:20:33 +0100

    config test uni

---
+++ tests/Nespresso/Test/ExpectedErrorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Nespresso\Test;
+
+class ExpectedErrorTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+      @expectedException PHPUnit_Framework_Error
+     */
+    public function testEchecInclude()
+    {
+	include 'fichier_qui_n_existe_pas.php';
+    }
+
+}
